ba4235ea2f7573b1ed0d5c476ab0764f7568ce40
Maxspell <m.stizhko@gmail.com>
2025-05-02 14:42:15 +0300

    Add sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,86 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const burgerBtn = document.querySelector('header button.burger');
-    const burgerBtnIcons = document.querySelectorAll('header button.burger img');
+
+  const burgerBtn = document.querySelector('header button.burger');
+  const burgerBtnIcons = document.querySelectorAll('header button.burger img');
+
+  burgerBtn.addEventListener('click', function() {
+    burgerBtnIcons.forEach((icon) => {
+      icon.classList.toggle('hidden');
+    });
+
+    document.querySelector('header .mobile-menu .nav').classList.toggle('visible');
+  })
+
+  const searchBtn = document.querySelector('header button.search-btn');
+
+  searchBtn.addEventListener('click', function() {
+    document.querySelector('header .mobile-menu .features .search').classList.toggle('expanded');
+  })
+
+  const loadMoreProductCards = document.getElementById('load-more-cards');
+  const productCards = document.querySelectorAll('.sec-4-card');
+  let visibleCards = getVisibleCardValue();
+  let activeVisibleCards = visibleCards;
+  let cardToOpen = visibleCards / 2;
+
+  function getVisibleCardValue() {
+    return Array.from(productCards).filter(card => getComputedStyle(card).display !== 'none').length
+  }
+
+  function debounce(func, wait = 100) {
+    let timeout;
+    return function() {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            func.apply(this, arguments);
+        }, wait);
+    };
+  }
+
+  function showCards() {
+    const remainingCards = productCards.length - activeVisibleCards;
+    const cardsToShow = Math.min(cardToOpen, remainingCards);
   
-    burgerBtn.addEventListener('click', function() {
-      burgerBtnIcons.forEach((icon) => {
-        icon.classList.toggle('hidden');
-      });
+    for (let i = activeVisibleCards; i < activeVisibleCards + cardsToShow && i < productCards.length; i++) {
+      productCards[i].style.display = 'block';
+    }
   
-      document.querySelector('header .mobile-menu .nav').classList.toggle('visible');
-    })
+    activeVisibleCards += cardsToShow;
+
+    if (activeVisibleCards >= productCards.length) {
+      loadMoreProductCards.querySelector('span').textContent = 'Load Less';
+    }
+  }
+
+  function hideCards() {
+    activeVisibleCards = visibleCards;
   
-    const searchBtn = document.querySelector('header button.search-btn');
-  
-    searchBtn.addEventListener('click', function() {
-      document.querySelector('header .mobile-menu .features .search').classList.toggle('expanded');
-    })
+    for (let i = productCards.length - 1; i >= visibleCards; i--) {
+      productCards[i].style.display = 'none';
+    }
+
+    loadMoreProductCards.querySelector('span').textContent = 'Load More';
+
+    const offset = 60;
+    const section = document.querySelector('.sec-4');
+    const sectionPosition = section.getBoundingClientRect().top + window.scrollY;
+    window.scrollTo({
+      top: sectionPosition + offset,
+      behavior: 'smooth'
+    });
+  }
+
+  window.addEventListener('resize', debounce(() => {
+    visibleCards = getVisibleCardValue();
+    activeVisibleCards = visibleCards;
+    cardToOpen = visibleCards / 2;
+  }, 200));
+
+  loadMoreProductCards.addEventListener('click', function() {
+    if (activeVisibleCards < productCards.length) {
+      showCards();
+    } else {
+      hideCards();
+    }
+  });
 });
